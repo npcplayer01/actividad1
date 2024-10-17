97deeb81746a8ef5e89512ef518d32e7d10aee23
Oscar Mauricio <dev_deep0@proton.me>
2024-10-17 12:44:46 -0600

    Ejercicio

---
+++ ISNP001724_UNIDAD1_1/ISNP001724_UNIDAD1_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISNP001724_UNIDAD1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            do
+            {
+                Console.Write("Introduce un número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero < 0)
+                {
+                    Console.WriteLine("El número es negativo.");
+                }
+                else if (numero > 0)
+                {
+                    Console.WriteLine("El número es positivo.");
+                }
+            } while (numero != 0);
+
+            Console.WriteLine("El número es 0, el programa se detendrá...");
+            Console.ReadLine();
+        }
+    }
+}
